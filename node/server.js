660ff5623043d53bf290c07acee5183cd5d7a5a2
envyoon <krafftdj@daum.net>
2025-07-27 23:52:51 +0900

    node express 기본 구조 연습

--- node/server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  res.end('hello from native http');
-});
-
-server.listen(3000, () => {
-  console.log('Native HTTP Server listening on port 3000');
-});
