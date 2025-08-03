6d7251d22f7fdf6782bc366648ee4183bd1d076c
envyoon <krafftdj@daum.net>
2025-08-03 21:35:19 +0900

    promise aysnc 버튼 제작 완료

--- my-app/public/javascripts/test.js
+++ my-app/public/javascripts/test.js
@@ -1,17 +1,87 @@
-function scwin_btn_onclick(value) {
 
-    if(value === 'pro'){
-        console.log("promise click");        
+/**
+ * '어싱크 버튼' 클릭 시 public/data/userList.json에 있는 데이터를 
+ * 'async/await' 방식으로 가져오는 기능입니다.
+ * id 값이 홀수인 데이터만 가지고 옵니다.
+ */
+async function btn_async_onclick() {
+    
+    /**
+     * json파일 읽어들이는게 fetch가 기본인 것 같은데,
+     * 라인수가 여러 줄 눌어나서이게 맞나 .. 를 생각해 보다가 
+     * axios를 사용하면 좀 더 편하하다고 찾아보았음. (난 여지껏 모르고 쓰고있었군..)
+     * 여기에서는 axios 사용해서 진행하는 것으로 하겠음.
+     */
 
+    console.log("async click");
+
+    try{
+        const response = await axios.get('/data/userList.json');
+        const userJson = response.data.users;
+        const oddUser = userJson.filter(user => user.id % 2 !== 0);
+
+        console.log('응답값 : ',response);
+        console.log('전체값 : ',userJson);
+        console.log('홀수값 : ',oddUser);
+
+        const html = oddUser.map(user => 
+            `<div>
+                ${user.id} : ${user.name} (${user.email})
+            </div>`
+        ).join('');
+
+        const val = oddUser.map(user =>
+            `${user.id} : ${user.name} (${user.email})`
+        ).join('\n');
+
+        document.getElementById("userList").innerHTML = html;
+        document.getElementById("showInfo").value = val;
+        document.getElementById("btnName").innerHTML = 'aysnc/await Button click!';
+        }
+    catch(e){
+        console.error(e);
+        console.error('통신 에러');
     }
-    
-    if(value === 'asy'){
-        console.log("async click");
-    }
 
-    const elem = document.getElementById("showInfo");
-    
+};
 
-    // console.log(elem);
+/**
+ * '프로미스 버튼' 클릭 시 public/data/userList.json에 있는 데이터를 
+ * 'Promise' 방식으로 가져오는 기능입니다.
+ * id 값이 홀수인 데이터만 가지고 옵니다.
+ */
+function btn_promise_onclick() {
     
-}
+    console.log("promise click");
+
+    const response = axios.get('/data/userList.json')
+        .then(function(res) {
+            console.log('응답값 : ',res);
+
+            const userJson = res.data.users;
+            const oddUser = userJson.filter(user => user.id % 2 !== 0);
+
+            console.log('전체값 : ',userJson);
+            console.log('홀수값 : ',oddUser);
+
+            const html = oddUser.map(user => 
+                `<div>
+                    ${user.id} : ${user.name} (${user.email})
+                </div>`
+            ).join('');
+        
+            const val = oddUser.map(user =>
+                `${user.id} : ${user.name} (${user.email})`
+            ).join('\n');
+        
+            document.getElementById("userList").innerHTML = html;
+            document.getElementById("showInfo").value = val;
+            document.getElementById("btnName").innerHTML = 'Promise Button click!';
+
+        })
+        .catch(function(e){
+            console.error(e);
+            console.error('통신 에러');
+        });
+    
+};
