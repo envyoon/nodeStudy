6bc60bc94b1687d0bbcabe5bc40fb350c9864f12
YOON\user <krafftdj@daum.net>
2025-08-04 13:20:10 +0900

    3초마다 role이 viewer인 데이터 불러오는 로직 제작 (interval)

--- my-app/public/javascripts/interval.js
+++ my-app/public/javascripts/interval.js
@@ -1,15 +1,21 @@
 
 /**
- * 
+ * localhost:3000/interval 경로로 접근 시
+ * 3초마다 roles 이 viewer 인 유저 정보를 불러오는 기능입니다.
  */
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async() => {
+
+    const response = await axios.get('/data/userList.json');
+    const userJson = response.data.users;
+    const getViewer = userJson.filter((user) => user.roles.includes('viewer'));
+
     const h2Name = document.getElementById('h2Name');
     let num = 0;
-    h2Name.textContent = `${num}`;
     
     setInterval(() => {
-        h2Name.textContent = num++;
-        console.log(`${num} 3초 마다 실행 됨.`);
+        h2Name.innerHTML = getViewer.map(user =>
+            `<div>${num++} | ${user.id} | ${user.name} | ${user.email} | ${user.isActive} | ${user.roles}</div>`
+        ).join('\n'); 
     }, 3000);
 
 });
