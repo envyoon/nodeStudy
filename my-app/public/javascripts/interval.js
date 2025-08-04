49eeebaa7157a54a72ee76467b091559bc2a7649
YOON\user <krafftdj@daum.net>
2025-08-04 15:49:38 +0900

    호출 정지 버튼 및 로직 생성

--- my-app/public/javascripts/interval.js
+++ my-app/public/javascripts/interval.js
@@ -3,6 +3,8 @@
  * localhost:3000/interval 경로로 접근 시
  * 3초마다 roles 이 viewer 인 유저 정보를 불러오는 기능입니다.
  */
+let intervalId;
+
 document.addEventListener('DOMContentLoaded', async() => {
 
     const response = await axios.get('/data/userList.json');
@@ -12,10 +14,24 @@ document.addEventListener('DOMContentLoaded', async() => {
     const h2Name = document.getElementById('h2Name');
     let num = 0;
     
-    setInterval(() => {
+    intervalId = setInterval(() => {
         h2Name.innerHTML = getViewer.map(user =>
             `<div>${num++} | ${user.id} | ${user.name} | ${user.email} | ${user.isActive} | ${user.roles}</div>`
         ).join('\n'); 
     }, 3000);
 
 });
+
+const stopCall = () => {
+    console.log('호출 정지 버튼 클릭 됨');
+
+    if(intervalId){
+        clearInterval(intervalId);
+        intervalId = null;
+        console.log("호출 정지 됨.")
+    }
+
+    const h2Name = document.getElementById('h2Name');
+    h2Name.textContent = '호출이 정지되었습니다.'
+
+};
